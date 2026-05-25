51d521eadfad1e4ea352d44f68b087c1ba79c6c2
Saif Ali <saifalipasha003@gmail.com>
2026-05-25 14:37:45 +0530

    Update server.js

--- Backened/server.js
+++ Backened/server.js
@@ -3,13 +3,29 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./Config/db.js";
 import productRouter from "./Routes/productRoute.js";
+
 const app = express();
+
 dotenv.config();
+
 const port = process.env.PORT || 4000;
+
+// Middleware
 app.use(cors());
 app.use(express.json());
-app.use("/api/product",productRouter);
+
+// Home Route
+app.get("/", (req, res) => {
+  res.send("Product Management API is Running 🚀");
+});
+
+// Product Routes
+app.use("/api/product", productRouter);
+
+// Database Connection
 connectDB();
-app.listen(port,()=>{
-    console.log(`Server run on port ${port}`);
+
+// Server Start
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
